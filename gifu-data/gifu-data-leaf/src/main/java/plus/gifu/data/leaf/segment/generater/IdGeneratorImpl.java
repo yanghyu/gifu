47f9a0b60b9c5ad38beb8e9d8b03ee9d91a7ad12
yanghongyu <869752070@qq.com>
2020-08-26 20:39:22 +0800

    修改

--- gifu-data/gifu-data-leaf/src/main/java/plus/gifu/data/leaf/segment/generater/IdGeneratorImpl.java
+++ gifu-data/gifu-data-leaf/src/main/java/plus/gifu/data/leaf/segment/generater/IdGeneratorImpl.java
@@ -1,5 +1,7 @@
 package plus.gifu.data.leaf.segment.generater;
 
+import plus.gifu.data.leaf.segment.exception.NoSuchKeyException;
+import plus.gifu.data.leaf.segment.exception.TooManyLoopException;
 import plus.gifu.data.leaf.segment.handler.SequenceHandler;
 import plus.gifu.data.leaf.segment.model.IdResult;
 import plus.gifu.data.leaf.segment.model.SequenceSegment;
@@ -20,9 +22,16 @@ public class IdGeneratorImpl implements IdGenerator {
 
     @Override
     public IdResult generateId(String key) {
-        SequenceSegment sequenceSegment = sequenceHandler.getSegment(key, 5);
+        SequenceSegment sequenceSegment = null;
         IdResult idResult = new IdResult();
-        idResult.setId(sequenceSegment.getMaxId());
+        try {
+            sequenceSegment = sequenceHandler.getSegment(key, 5);
+            idResult.setId(sequenceSegment.getMaxId());
+        } catch (NoSuchKeyException e) {
+            e.printStackTrace();
+        } catch (TooManyLoopException e) {
+            e.printStackTrace();
+        }
         return idResult;
     }
 
